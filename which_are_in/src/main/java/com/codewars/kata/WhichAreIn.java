980e0fedbf45874188c81a2143cedf89e881e10c
Sao Paulo Boco <saoprb@gmail.com>
2016-12-22 11:47:53 +0800

    Add solved kata 'Which are in?'.

---
+++ which_are_in/src/main/java/com/codewars/kata/WhichAreIn.java
@@ -0,0 +1,46 @@
+package com.codewars.kata;
+
+/**
+ * Created by saoprb on 12/22/16.
+ */
+public class WhichAreIn {
+    public static String[] inArray(String[] a, String[] b) {
+        java.util.HashSet<String> matched = new java.util.HashSet<String>();
+        for (String key : a) {
+            for (String word : b) {
+                if (word.contains(key)) {
+                    matched.add(key);
+                }
+            }
+        }
+        java.util.ArrayList<String> sortedMatched = new java.util.ArrayList<String>(matched);
+        java.util.Collections.sort(sortedMatched, new java.util.Comparator<String>() {
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        return sortedMatched.toArray(a);
+    }
+
+    public static String[] inArrayLambda(String[] a, String[] b) {
+        java.util.ArrayList<String> listA = new java.util.ArrayList<>(java.util.Arrays.asList(a));
+
+        class WordGroup {
+            String[] group;
+            public WordGroup(String[] group) {
+                this.group = group;
+            }
+            public boolean contains(String key) {
+                for (String candidate : group) {
+                    if (candidate.contains(key))
+                        return true;
+                }
+                return false;
+            }
+        }
+
+        WordGroup group = new WordGroup(b);
+
+        return listA.stream().filter(key -> group.contains(key)).distinct().sorted().toArray(String[]::new);
+    }
+}
---
+++ which_are_in/src/test/java/com/codewars/kata/WhichAreInTest.java
@@ -0,0 +1,26 @@
+package com.codewars.kata;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * Created by saoprb on 12/22/16.
+ */
+public class WhichAreInTest {
+    @Test
+    public void test1() {
+        String a[] = new String[]{ "arp", "live", "strong" };
+        String b[] = new String[] { "lively", "alive", "harp", "sharp", "armstrong" };
+        String r[] = new String[] { "arp", "live", "strong" };
+        assertArrayEquals(r, WhichAreIn.inArray(a, b));
+    }
+
+    @Test
+    public void test2() {
+        String a[] = new String[]{ "arp", "live", "strong" };
+        String b[] = new String[] { "lively", "alive", "harp", "sharp", "armstrong" };
+        String r[] = new String[] { "arp", "live", "strong" };
+        assertArrayEquals(r, WhichAreIn.inArrayLambda(a, b));
+    }
+}
