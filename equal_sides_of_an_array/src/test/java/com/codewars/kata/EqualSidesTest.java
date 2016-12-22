c08e1f2901b8ff5db7412273f6a58f4e0d1f43df
Sao Paulo Boco <saoprb@gmail.com>
2016-12-23 06:19:04 +0800

    Change junit asserts to hamcrest to make it more readable.

--- equal_sides_of_an_array/src/test/java/com/codewars/kata/EqualSidesTest.java
+++ equal_sides_of_an_array/src/test/java/com/codewars/kata/EqualSidesTest.java
@@ -2,7 +2,10 @@ package com.codewars.kata;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by saoprb on 12/20/16.
@@ -10,12 +13,12 @@ import static org.junit.Assert.assertEquals;
 public class EqualSidesTest {
     @Test
     public void test() {
-        assertEquals(3,EqualSides.findEvenIndex(new int[] {1,2,3,4,3,2,1}));
-        assertEquals(1,EqualSides.findEvenIndex(new int[] {1,100,50,-51,1,1}));
-        assertEquals(-1,EqualSides.findEvenIndex(new int[] {1,2,3,4,5,6}));
-        assertEquals(3,EqualSides.findEvenIndex(new int[] {20,10,30,10,10,15,35}));
-        assertEquals(-1,EqualSides.findEvenIndex(new int[] {-8505, -5130, 1926, -9026}));
-        assertEquals(1,EqualSides.findEvenIndex(new int[] {2824, 1774, -1490, -9084, -9696, 23094}));
-        assertEquals(6,EqualSides.findEvenIndex(new int[] {4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4}));
+        assertThat(EqualSides.findEvenIndex(new int[] {1,2,3,4,3,2,1}), is(equalTo(3)));
+        assertThat(EqualSides.findEvenIndex(new int[] {1,100,50,-51,1,1}), is(equalTo(1)));
+        assertThat(EqualSides.findEvenIndex(new int[] {1,2,3,4,5,6}), is(equalTo(-1)));
+        assertThat(EqualSides.findEvenIndex(new int[] {20,10,30,10,10,15,35}), is(equalTo(3)));
+        assertThat(EqualSides.findEvenIndex(new int[] {-8505, -5130, 1926, -9026}), is(equalTo(-1)));
+        assertThat(EqualSides.findEvenIndex(new int[] {2824, 1774, -1490, -9084, -9696, 23094}), is(equalTo(1)));
+        assertThat(EqualSides.findEvenIndex(new int[] {4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4}), is(equalTo(6)));
     }
 }
